a3a96e8b849531755c922b51eda8bc4b8ea94bd8
harry <knight65210@gmail.com>
2023-06-23 17:24:21 +0800

    1.商品功能-設定返回的時間格式和時間 2.新增ProductCategory-Enum，並把category類別改成使用Enum

---
+++ src/main/java/com/kaoharry/springbootbookstore/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.kaoharry.springbootbookstore.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- src/main/java/com/kaoharry/springbootbookstore/rowmapper/ProductRowMapper.java
+++ src/main/java/com/kaoharry/springbootbookstore/rowmapper/ProductRowMapper.java
@@ -2,6 +2,7 @@ package com.kaoharry.springbootbookstore.rowmapper;
 
 import com.kaoharry.springbootbookstore.model.Product;
 import org.springframework.jdbc.core.RowMapper;
+import com.kaoharry.springbootbookstore.constant.ProductCategory;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,13 +13,24 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+
+//        product.setCategory(rs.getString("category"));
+//      String 轉 Enum
+        String categoryStr = rs.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+
+//      簡寫：product.setCategory(ProductCategory.valueOf(rs.getString("category")));
+
+
+
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
         product.setDescription(rs.getString("description"));
-        product.setCreatedData(rs.getDate("created_date"));
-        product.setLastModifiedDate(rs.getDate("last_modified_date"));
+        product.setCreatedData(rs.getTimestamp("created_date"));
+        product.setLastModifiedDate(rs.getTimestamp("last_modified_date"));
         return product;
     }
 }
